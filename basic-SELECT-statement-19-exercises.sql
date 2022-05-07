9b9a6c258ba80952b7c69ebf73539013e9937208
Luiz Felipe da Silveira <l.filiphis@hotmail.com>
2022-05-07 11:17:48 -0300

    :wrench: Adicionando o banco de dados que sera utilizado e a primeira lista de exercicios

---
+++ basic-SELECT-statement-19-exercises.sql
@@ -0,0 +1,12 @@
+-- SELECT básico [19 exercícios]
+
+-- 1) Escreva uma consulta para exibir os nomes (first_name, last_name) da tabela employees usando os alias "First Name", "Last Name". 
+SELECT
+	FIRST_NAME AS "First Name",
+    LAST_NAME AS "Last Name"
+FROM
+	employees
+;
+
+-- 
+
