69e4b0fe251b8f775d78148961d13b0ea4b7339b
Luiz Felipe da Silveira <l.filiphis@hotmail.com>
2022-05-09 23:04:30 -0300

    :wrench: Iniciando a Lista 03 - Funções de Agregação e Group By

---
+++ aggregate-functions-and-group-by.sql
@@ -0,0 +1,29 @@
+-- Lista 03 - Funções de Agregação e Group By [14 exercícios]
+-- Link: https://www.w3resource.com/mysql-exercises/aggregate-function-exercises/
+
+-- 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
